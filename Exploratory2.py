3e084a320b41d874882bbb60d5db0f75d30aa798
Trai <tdg52@scarletmail.rutgers.edu>
2019-10-15 13:05:42 -0400

    created a list function and called it

---
+++ Exploratory2.py
@@ -0,0 +1,10 @@
+# defined an argument as a list
+# created a TypeError for non-lists
+def greerboys(numbers)
+if type(greerboys) != list:
+  raise TypeError("not the boys")
+for element in numbers:
+  if element >= 0:
+    return yes
+greerboys([1, 2, 3])
+greerboys("not the boys")
